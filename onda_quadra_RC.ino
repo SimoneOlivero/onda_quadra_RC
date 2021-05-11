4c4aa268375af86fa2c69d071eadb73237f6171f
Simone Olivero <83812963+SimoneOlivero@users.noreply.github.com>
2021-05-11 23:17:02 +0200

    Update onda_quadra_RC.ino

--- onda_quadra_RC.ino
+++ onda_quadra_RC.ino
@@ -6,17 +6,15 @@ void setup() {
 }
 
 void loop() {
-  // utlizzo l'input analogico A4 come starer (quando premo il pulsante rileva una tensione e farà partire il programma)
+  // utilizzo l'input analogico A4 come starter (quando premo il pulsante rileva una tensione e farà partire il programma)
   float start = analogRead(A4)*5/1024.;
   if (start > 3.0) {
     unsigned long t0 = micros(); // rilevo il momento in cui inizia il cilo
     unsigned long t = 700; // t é uguale al periodo/2
    
+    for(int T=0;T<3;T++) { // il numero di periodi (3) per cui diamo in uscita l'onda quadra
     
-    // 
-    for(int T=0;T<3;T++) { // il numero di periodi (5) per cui diamo in uscita l'onda quadra
-    
-      digitalWrite(13, HIGH); // imposto il pin 13 su HIGH, fronira una tensione di 5V
+      digitalWrite(13, HIGH); // imposto il pin 13 su HIGH, fronirà una tensione di 5V
       unsigned long startH = millis(); // rilevo il momento in cui il pin 13 passa allo stato HIGH
 
       // l'arduino stampa nel monitor seriale i dati della tensione sul PIN 13 e ai capi del condensatore finchè il tempo trascorso è minore di t impostato in precedenza
@@ -30,7 +28,7 @@ void loop() {
         float voltageC = analogRead(A0)*5/1024.;
         Serial.println(voltageC);    
       }
-      digitalWrite(13, LOW); // imposto il pin 13 su LOW, fronira una tensione di 0V
+      digitalWrite(13, LOW); // imposto il pin 13 su LOW, fronirà una tensione di 0V
       unsigned long startL = millis(); // rilevo il momento in cui il pin 13 passa allo stato LOW
       
       // l'arduino stampa nel monitor seriale i dati della tensione sul PIN 13 e ai capi del condensatore finchè il tempo trascorso è minore di t impostato in precedenza
